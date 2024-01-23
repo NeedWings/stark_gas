8e52477a54cc09c17af6518cee544a2abaecc421
NeedWings <136832174+NeedWings@users.noreply.github.com>
2024-01-23 13:34:31 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from starknet_py.net.full_node_client import FullNodeClient
-
+import asyncio
  
 client = FullNodeClient("https://starknet-mainnet.public.blastapi.io")
 async def main():
     while True:
-        print((await client.get_block("LATEST")).gas_price/1e9)
+        print((await client.get_block("latest")).l1_gas_price.price_in_wei/1e9)
         await asyncio.sleep(1)
 
 asyncio.run(main())
